bbfc31e893c6275cd4ce99363bf8bf7dfad4678d
Gabriel Nikol <gabriel-martin.nikol@stud.uni-bamberg.de>
2017-12-02 17:56:48 +0100

    merged main.cpp source code from ArduinoSockets with RaspberryPiSockets

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,15 @@
+// nrf95_ping_pong.ino
+// -*- mode: C++ -*-
+
+#ifdef ESP8266
+#include <ESP8266WiFi.h>
+#endif
+
+#include <Arduino.h>
+#ifdef Arduino_h
+#include <SPI.h>
+#include <Ethernet.h>
+#endif
 
 #ifndef Arduino_h
 #include <dirent.h>
@@ -8,27 +20,32 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <string>
+#include <LoggerInterface.h>
+SerialLinux Serial;
 #endif
 
-#include <LoggerInterface.h>
+#include <LinuxLogger.h>
 #include <MqttSnMessageHandler.h>
+#include <LinuxLogger.h>
 #include <RF95Socket.h>
 #include <RH_RF95.h>
 #include <RHReliableDatagram.h>
+
 
 RF95Socket socket;
 LoggerInterface logger;
 MqttSnMessageHandler mqttSnMessageHandler;
 
+#ifdef ESP8266
+RH_RF95 rf95(2, 15);
+#else
 RH_RF95 rf95;
+#endif
 RHReliableDatagram manager(rf95);
 
-#ifndef Arduino_h
-SerialLinux Serial;
-#endif
-
 #ifdef PING
-#define OWN_ADDRESS 0x02
+#define OWN_ADDRESS 0x05
 #define PONG_ADDRESS 0x03
 device_address target_address(PONG_ADDRESS, 0, 0, 0, 0, 0);
 uint8_t msg[] = {5, 'P', 'i', 'n', 'g'};
@@ -37,10 +54,24 @@ uint8_t msg[] = {5, 'P', 'i', 'n', 'g'};
 #endif
 
 
-
 void setup() {
     Serial.begin(9600);
     Serial.println("Starting");
+    Serial.print("OWN_ADDRESS: ");
+#ifndef Arduino_h
+    std::string number_str = std::to_string(OWN_ADDRESS);
+    Serial.println(number_str.c_str());
+#else
+    Serial.println(OWN_ADDRESS);
+#endif
+    Serial.print("ROLE: ");
+#ifdef PING
+    Serial.println("PING");
+#elif PONG
+    Serial.println("PONG");
+#else
+    Serial.println("UNDEFINED");
+#endif
 
 #ifndef Arduino_h
     wiringPiSetupGpio();
@@ -60,13 +91,12 @@ void setup() {
         Serial.println("Started");
     }
 #ifdef PING
-        mqttSnMessageHandler.send(&target_address, msg, (uint16_t) msg[0]);
+    mqttSnMessageHandler.send(&target_address, msg, (uint16_t) msg[0]);
 #ifdef RH_RF95_h
-        mqttSnMessageHandler.send(&target_address, msg, (uint16_t) msg[0]);
+    mqttSnMessageHandler.send(&target_address, msg, (uint16_t) msg[0]);
 #endif
 #endif
 }
-
 
 void loop() {
     mqttSnMessageHandler.loop();
@@ -96,4 +126,3 @@ int main(int argc, char **argv) {
     return EXIT_SUCCESS;
 }
 #endif
-
